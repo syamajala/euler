d15601a22d8af0c41d12c5c5927bdf3c46e71751
syamajala <syamajala@gmail.com>
2014-07-31 18:00:04 -0400

    Solutions for problems 6 and 22.

---
+++ p22.py
@@ -0,0 +1,24 @@
+import string
+
+
+class namelist():
+
+    def __init__(self):
+        with open("p22.txt", 'r') as f:
+            l = f.readline()
+            names = l.split(',')
+        names = map(lambda x: x.strip('"'), names)
+        names.sort()
+        self.names = names
+
+    def name_value(self, n):
+        v = map(lambda x: string.ascii_uppercase.index(x) + 1, list(n))
+        return sum(v)*(self.names.index(n)+1)
+
+
+def main():
+    n = namelist()
+    s = map(n.name_value, n.names)
+    print sum(s)
+
+main()
---
+++ p6.py
@@ -0,0 +1,9 @@
+def main():
+    sum_of_squares = map(lambda x: x*x, range(1, 101))
+    sum_of_squares = sum(sum_of_squares)
+
+    square_of_sum = sum(range(1, 101))**2
+
+    print square_of_sum - sum_of_squares
+
+main()
